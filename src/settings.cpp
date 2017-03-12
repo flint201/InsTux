1ab346e367c308c332e69b2d1f3cff8153919c3e
Flint <flintlock201@gmail.com>
2017-03-12 14:07:52 -0500

    Changed default values for recoil cancellation

--- src/settings.cpp
+++ src/settings.cpp
@@ -11,8 +11,8 @@ float Settings::Aimbot::cont_thresh = 5.0;
 
 float Settings::Aimbot::silent_fov = 0.5;
 float Settings::Aimbot::silent_fov_hip = 60;
-float Settings::Aimbot::recoilx = 2.0;
-float Settings::Aimbot::recoily = 1.5;
+float Settings::Aimbot::recoilx = 1.5;
+float Settings::Aimbot::recoily = 1.0;
 
 bool Settings::Cham::only_on_key_down = false;
 ButtonCode_t Settings::Cham::key = KEY_CAPSLOCK;
